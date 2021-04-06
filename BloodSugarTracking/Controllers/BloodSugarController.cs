ba42b19577f9f506f500f125b84fc95c04f3bca3
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-04-06 17:40:08 +0530

    wip

--- BloodSugarTracking/Controllers/BloodSugarController.cs
+++ BloodSugarTracking/Controllers/BloodSugarController.cs
@@ -2,6 +2,7 @@
 using BloodSugarTracking.Models;
 using BloodSugarTracking.Options;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using System.Linq;
 using System.Threading.Tasks;
@@ -47,6 +48,85 @@ namespace BloodSugarTracking.Controllers
             _bloodSugarContext.BloodSugarTestResults!.Add(bloodSugarTestResult);
             await _bloodSugarContext.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bloodSugarTestResult = _bloodSugarContext.BloodSugarTestResults!.Find(id);
+            if (bloodSugarTestResult == null)
+            {
+                return NotFound();
+            }
+            return View(bloodSugarTestResult);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, BloodSugarTestResult bloodSugarTestResult)
+        {
+            if (id != bloodSugarTestResult.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _bloodSugarContext.BloodSugarTestResults!.Update(bloodSugarTestResult);
+                    _bloodSugarContext.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BloodSugarTestResultExists(bloodSugarTestResult.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bloodSugarTestResult);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = _bloodSugarContext.BloodSugarTestResults!
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var student = _bloodSugarContext.BloodSugarTestResults!.Find(id);
+            _bloodSugarContext.BloodSugarTestResults!.Remove(student);
+            _bloodSugarContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BloodSugarTestResultExists(int id)
+        {
+            return _bloodSugarContext.BloodSugarTestResults!.Any(e => e.Id == id);
         }
     }
 }
