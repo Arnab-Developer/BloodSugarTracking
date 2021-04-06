ae7b538a622424b78d64c4aa31df20955e1923cf
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-04-06 15:22:20 +0530

    wip

--- BloodSugarTracking/Models/BloodSugarTestResult.cs
+++ BloodSugarTracking/Models/BloodSugarTestResult.cs
@@ -1,6 +1,4 @@
-﻿using BloodSugarTracking.Options;
-using Microsoft.Extensions.Options;
-using System;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace BloodSugarTracking.Models
