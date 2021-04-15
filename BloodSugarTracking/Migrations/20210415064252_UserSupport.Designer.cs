61b4c9e71eabeeaa63fc8f19cffc8324c4153840
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-04-15 12:22:26 +0530

    add user migrations

--- BloodSugarTracking/Migrations/20210415064252_UserSupport.Designer.cs
+++ BloodSugarTracking/Migrations/20210415064252_UserSupport.Designer.cs
@@ -4,14 +4,16 @@ using BloodSugarTracking.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BloodSugarTracking.Migrations
 {
     [DbContext(typeof(BloodSugarContext))]
-    partial class BloodSugarContextModelSnapshot : ModelSnapshot
+    [Migration("20210415064252_UserSupport")]
+    partial class UserSupport
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BloodSugarTracking/Migrations/20210415064252_UserSupport.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BloodSugarTracking.Migrations
+{
+    public partial class UserSupport : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "BloodSugarTestResults",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BloodSugarTestResults_UserId",
+                table: "BloodSugarTestResults",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BloodSugarTestResults_Users_UserId",
+                table: "BloodSugarTestResults",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BloodSugarTestResults_Users_UserId",
+                table: "BloodSugarTestResults");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BloodSugarTestResults_UserId",
+                table: "BloodSugarTestResults");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "BloodSugarTestResults");
+        }
+    }
+}
